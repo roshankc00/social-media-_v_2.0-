781c5df080fc22cd1cf924f74cffab1bbea553b3
roshankc00 <120471898+roshankc00@users.noreply.github.com>
2023-10-05 01:50:51 +0545

    feat:connect the database

---
+++ prisma/migrations/20231004200415_added_user_schema/migration.sql
@@ -0,0 +1,13 @@
+-- CreateTable
+CREATE TABLE "User" (
+    "id" TEXT NOT NULL,
+    "firstName" TEXT NOT NULL,
+    "lastName" TEXT,
+    "email" TEXT NOT NULL,
+    "password" TEXT,
+    "profileImageUrl" TEXT,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "User_pkey" PRIMARY KEY ("id")
+);
